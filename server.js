2b732e345b21298bd8f940d085b1a6bf27d812b5
Sourabh Singh <ashusingh169696@gmail.com>
2022-07-20 01:24:26 +0530

    update

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const data =require("./db.json")
 const jsonServer =require("json-server");
 const server = jsonServer.create();
-const router=jsonServer.router(data);
-const middleware =jsonServer.default();
+const router=jsonServer.router("./db.json");
+const middleware =jsonServer.default({
+    static:"./build"
+});
 const PORT =process.env.PORT|| 5000;
 server.use(middleware);
-server.use(router)
+server.use(
+    jsonServer.rewriter({
+        "/api/*":"/$1",
+    })
+);
+
+server.use(router);
 server.listen(PORT,()=>{
     console.log("Server is running");
 });
